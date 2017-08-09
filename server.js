9b2c91a05c37f2c3713d7ac2eb779501e4240ab0
Jay <techjp9334@gmail.com>
2017-08-09 18:40:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname,'ui','article-one')); 
+app.get('/article-one',function(req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
 });
 app.get('/article-two',function(req,res){
    res.send('Article two is requested so it is being served'); 
